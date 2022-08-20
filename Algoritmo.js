b982f21fce05d11c225059aff5ed3bb8c8ca67c3
Ernesto Corona <ernesto_rodriguezc2002@outlook.com>
2022-08-20 14:30:32 -0700

    ya es una version usable

---
+++ Algoritmo.js
@@ -0,0 +1,105 @@
+document.getElementById("div-aparece").style.display = 'none';
+inputverificar();
+function saltarLinea ()
+{
+    document.write("<br>");
+}
+function imprimir (mensaje)
+{
+    texto.value = mensaje;
+}
+// instanciamos los objetos que utilizamos en html para poder recopilar la informacion en jss
+var texto = document.querySelector("#mensaje");
+var input = document.querySelector("#textBox");
+var fraseCodificada = "";
+var fraseDecodificada = "";
+texto.focus();
+
+function codigoEncriptado (letra)
+{
+var letraCodificada = letra;
+if (letra == 'a')
+{
+    letraCodificada = "ai";
+}
+if (letra == 'e')
+{
+    letraCodificada = "enter";
+}
+if (letra == 'i')
+{
+    letraCodificada = "imes";
+}
+if (letra == 'o')
+{
+    letraCodificada = "ober";
+}
+if (letra == 'u')
+{
+    letraCodificada = "ufat";
+}
+return letraCodificada;
+}
+
+function encriptar (datoEscrito)
+{
+    for (var i = 0; i < texto.value.length; i++)
+    {
+        fraseCodificada += codigoEncriptado(datoEscrito.value[i]);
+    }
+    return fraseCodificada;
+}
+
+function desencriptar ()
+{
+    fraseDecodificada = texto.value.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    return fraseDecodificada;
+}
+
+document.getElementById('botonEncriptar').onclick = (e) => {
+    e.preventDefault();
+    for (var i = 0; i < input.value.length; i++)
+        {
+            fraseCodificada += codigoEncriptado(input.value[i]);
+        }
+    mensaje.value = fraseCodificada;
+    input.value = "";
+    aparece()
+}
+
+document.getElementById('botonDesencriptar').onclick = (e) => {
+    e.preventDefault();
+    const textoDesencriptado = desencriptar(input.value);
+    mensaje.value = textoDesencriptado;
+    input.value = "";
+    aparece()
+}
+
+document.getElementById('btn-copiar').onclick = (e) => {
+    e.preventDefault();
+    const mensaje = document.querySelector("#mensaje");
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+}
+
+function aparece() {
+    document.getElementById("div-desaparece").style.display = 'none';
+    document.getElementById("div-aparece").style.display = 'block';
+}
+
+function inputverificar() {
+    var inputPalabra = document.querySelector("#textBox");
+    inputPalabra.addEventListener("keypress", function (e) {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+
+    if (keyCode > 47 && keyCode < 65) {
+    e.preventDefault();
+}
+});
+}
+
+function home() {
+    document.getElementById("div-aparece").style.display = 'none';
+    document.getElementById("div-desaparece").style.display = 'block';
+}
